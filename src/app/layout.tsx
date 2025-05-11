cf2a4590ffad018081b12c51c5f33589ad356955
Marcus V. Ximenes <marcus@marcusvinicius.info>
2025-05-11 20:04:38 -0300

    Configure Tailwind CSS and default font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,19 @@
+import type { Metadata } from "next";
 import { ReactNode } from "react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Orgalib",
+  description: "The book organization app",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>{children}</body>
     </html>
   );
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <h2 className="text-2xl font-semibold mb-4">Welcome!</h2>
+      <p>The Book Organization app!</p>
+    </Layout>
+  );
+}
